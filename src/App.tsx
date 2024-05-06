48c4b4c717f9e86bd068ee8020b7cca3f6d97ab4
jp7eph <jp7eph@gmail.com>
2024-05-06 15:06:45 +0900

    グルーピング（Component化前）

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import './App.css';
 import { useState } from 'react';
 import { Message } from './background';
 import { Comment } from './comment';
-import { AppBar, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Collapse, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 import CommentIcon from '@mui/icons-material/Comment';
 import DoneIcon from '@mui/icons-material/Done';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
 function App() {
   console.log('[ls-comments] open action!!');
 
-  const [comments, setComments] = useState<Comment[]>([]);
-
+  const [unResolvedComments, setUnResolvedComments] = useState<Comment[]>([]);
+  const [resolvedComments, setResolvedComments] = useState<Comment[]>([]);
   const message: Message = { type: 'getComments', comments: [] };
   chrome.runtime.sendMessage(message, (response: Comment[]) => {
     console.log('[ls-comments]', 'get comments', { response });
-    setComments(response);
+    setUnResolvedComments(response.filter((comment) => { return !comment.isResolved; }));
+    setResolvedComments(response.filter((comment) => { return comment.isResolved; }));
   });
+
+  const [openUnResolveds, setOpenUnResolveds] = useState(true);
+  const [openResolveds, setOpenResolveds] = useState(false);
+  const handleOpenUnResolveds = () => { setOpenUnResolveds(!openUnResolveds); };
+  const handleOpenResolveds = () => { setOpenResolveds(!openResolveds); };
 
   return (
     <>
@@ -24,31 +31,73 @@ function App() {
           <Typography variant='h6'>ls-comments</Typography>
         </Toolbar>
       </AppBar>
-      {comments == null || comments.length == 0 ? (
+      {unResolvedComments == null || unResolvedComments.length == 0 ? (
         <Typography variant='body1' sx={{ p: 2 }}>インラインコメントがありません</Typography>
       ) : (
-        <Box sx={{ maxHeight: 200, overflow: 'auto' }}>
+        <Box sx={{ height: 300, width: '100%', overflow: 'auto' }}>
           <List>
-            {
-              comments.map((comment) => (
-                <ListItem key={comment.refId} disablePadding>
-                  <ListItemButton onClick={() => {
-                    // 表示しているタブのcontent-scriptに対してコメントジャンプの指示を飛ばす
-                    chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
-                      if (tabs.length > 0 && tabs[0].id != undefined) {
-                        const message: Message = { type: 'jumpComments', comments: [comment] };
-                        chrome.tabs.sendMessage(tabs[0].id, message);
-                      }
-                    });
-                  }}>
-                    <ListItemIcon>
-                      {comment.isResolved ? (<DoneIcon color='success' />) : (<CommentIcon color='warning' />)}
-                    </ListItemIcon>
-                    <ListItemText primary={comment.bodyText} />
-                  </ListItemButton>
-                </ListItem>
-              ))
-            }
+            <ListItemButton onClick={handleOpenUnResolveds}>
+              <ListItemIcon>
+                <CommentIcon color='warning' />
+              </ListItemIcon>
+              <ListItemText primary={'未解決 (' + unResolvedComments.length + ')'} />
+              {openUnResolveds ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openUnResolveds} timeout="auto" unmountOnExit>
+              <List disablePadding>
+                {
+                  unResolvedComments.map((comment) => (
+                    <ListItem key={comment.refId} disablePadding>
+                      <ListItemButton sx={{ pl: 4 }} onClick={() => {
+                        // 表示しているタブのcontent-scriptに対してコメントジャンプの指示を飛ばす
+                        chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+                          if (tabs.length > 0 && tabs[0].id != undefined) {
+                            const message: Message = { type: 'jumpComments', comments: [comment] };
+                            chrome.tabs.sendMessage(tabs[0].id, message);
+                          }
+                        });
+                      }}>
+                        <ListItemIcon>
+                          {comment.isResolved ? (<DoneIcon color='success' />) : (<CommentIcon color='warning' />)}
+                        </ListItemIcon>
+                        <ListItemText primary={comment.bodyText} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                }
+              </List>
+            </Collapse>
+            <ListItemButton onClick={handleOpenResolveds}>
+              <ListItemIcon>
+                <DoneIcon color='success' />
+              </ListItemIcon>
+              <ListItemText primary={'解決済 (' + resolvedComments.length + ')'} />
+              {openResolveds ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openResolveds} timeout="auto" unmountOnExit>
+              <List disablePadding>
+                {
+                  resolvedComments.map((comment) => (
+                    <ListItem key={comment.refId} disablePadding>
+                      <ListItemButton sx={{ pl: 4 }} onClick={() => {
+                        // 表示しているタブのcontent-scriptに対してコメントジャンプの指示を飛ばす
+                        chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+                          if (tabs.length > 0 && tabs[0].id != undefined) {
+                            const message: Message = { type: 'jumpComments', comments: [comment] };
+                            chrome.tabs.sendMessage(tabs[0].id, message);
+                          }
+                        });
+                      }}>
+                        <ListItemIcon>
+                          {comment.isResolved ? (<DoneIcon color='success' />) : (<CommentIcon color='warning' />)}
+                        </ListItemIcon>
+                        <ListItemText primary={comment.bodyText} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))
+                }
+              </List>
+            </Collapse>
           </List >
         </Box>
       )
