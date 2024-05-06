7d195094776be50c14f7a7fa63cb9d38e1bdff5d
jp7eph <jp7eph@gmail.com>
2024-05-06 14:22:16 +0900

    Browser actionの実装ひとまず完了 ネスト化前

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import { Message } from './background';
 import { Comment } from './comment';
-import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { AppBar, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
 import CommentIcon from '@mui/icons-material/Comment';
 import DoneIcon from '@mui/icons-material/Done';
 
@@ -19,31 +19,38 @@ function App() {
 
   return (
     <>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>ls-comments</Typography>
+        </Toolbar>
+      </AppBar>
       {comments == null || comments.length == 0 ? (
-        <p>no comments</p>
+        <Typography variant='body1' sx={{ p: 2 }}>インラインコメントがありません</Typography>
       ) : (
-        <List>
-          {
-            comments.map((comment) => (
-              <ListItemButton key={comment.refId} onClick={() => {
-                // 表示しているタブのcontent-scriptに対してコメントジャンプの指示を飛ばす
-                chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
-                  if (tabs.length > 0 && tabs[0].id != undefined) {
-                    const message: Message = { type: 'jumpComments', comments: [comment] };
-                    chrome.tabs.sendMessage(tabs[0].id, message);
-                  }
-                });
-              }}>
-                <ListItemIcon>
-                  {comment.isResolved ? (<DoneIcon />) : (<CommentIcon />)}
-                </ListItemIcon>
-                <ListItem>
-                  <ListItemText primary={comment.bodyText}></ListItemText>
+        <Box sx={{ maxHeight: 200, overflow: 'auto' }}>
+          <List>
+            {
+              comments.map((comment) => (
+                <ListItem key={comment.refId} disablePadding>
+                  <ListItemButton onClick={() => {
+                    // 表示しているタブのcontent-scriptに対してコメントジャンプの指示を飛ばす
+                    chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+                      if (tabs.length > 0 && tabs[0].id != undefined) {
+                        const message: Message = { type: 'jumpComments', comments: [comment] };
+                        chrome.tabs.sendMessage(tabs[0].id, message);
+                      }
+                    });
+                  }}>
+                    <ListItemIcon>
+                      {comment.isResolved ? (<DoneIcon color='success' />) : (<CommentIcon color='warning' />)}
+                    </ListItemIcon>
+                    <ListItemText primary={comment.bodyText} />
+                  </ListItemButton>
                 </ListItem>
-              </ListItemButton>
-            ))
-          }
-        </List >
+              ))
+            }
+          </List >
+        </Box>
       )
       }
     </>
